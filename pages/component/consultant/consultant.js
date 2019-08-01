db78fc0521920a9f190ab955650ebd5b86fcf7cd
靳博昊 <2287650045@qq.com>
2019-08-01 11:47:03 +0800

    所有页面加上客服入口

---
+++ pages/component/consultant/consultant.js
@@ -0,0 +1,25 @@
+// pages/component/consultant/consultant.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+  externalClasses: ['my-class'],
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+  created:function(){
+    console.log("我金流了")
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
